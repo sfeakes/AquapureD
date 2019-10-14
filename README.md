10756f41be60fc43571b9b479dbe1396001a10be
sfeakes <sfeakes@gmail.com>
2019-10-14 09:47:41 -0500

    update

--- README.md
+++ README.md
@@ -17,12 +17,15 @@ Use of GPIO to control relays to turn on/off other pool equiptment.
 Homekit integration with homekit-aqualinkd
 
 ### Web API
+```
 http://ip/?command=status                   (return JSON status)
 http://ip/?command=swg_percent&value=50     (Set SWG Percent to 50)
 http://ip/?command=swg&value=on             (Set SWG on or off)
-http://ip/?command=GPIO_13&value=on          (Set GPIO 13 on or off)
+http://ip/?command=GPIO_13&value=on         (Set GPIO 13 on or off)
+```
 
 ### MQTT
+```
 #### Status posted.  0=off 1=on,
 aquapured/SWG 0
 aquapured/SWG/Percent 30
@@ -34,12 +37,13 @@ aquapured/GPIO_13 0
 aquapured/GPIO_19 0
 aquapured/GPIO_18 0
 aquapured/GPIO_16 0
-
+```
 #### Make requests over MQTT
+```
 Add /set to topic and messages are 0=off, 1=on or number between 1 and 100 for percent. 
 aquapured/SWG/set
 aquapured/SWG/Percent/set
 aquapured/SWG/Boost/set
 aquapured/GPIO_13/set
-
+```
 
--- ap_net_services.h
+++ ap_net_services.h
@@ -6,14 +6,15 @@
 #define GET_RTN_NOT_CHANGED "Not Changed"
 #define GET_RTN_ERROR "Error"
 
+bool start_net_services(struct mg_mgr *mgr);
+bool broadcast_aquapurestate(struct mg_connection *nc);
+void check_net_services(struct mg_mgr *mgr);
+
 //void main_server();
 //void main_server_TEST(struct aqualinkdata *aqdata, char *s_http_port);
 //bool start_web_server(struct mg_mgr *mgr, struct aqualinkdata *aqdata, char *port, char* web_root);
 //bool start_net_services(struct mg_mgr *mgr, struct aqualinkdata *aqdata, struct aqconfig *aqconfig);
 //bool start_net_services(struct mg_mgr *mgr, struct aqconfig *config, struct arconfig *ar_prms);
-bool start_net_services(struct mg_mgr *mgr);
-bool broadcast_aquapurestate(struct mg_connection *nc);
-void check_net_services(struct mg_mgr *mgr);
 //void broadcast_aqualinkstate(struct mg_connection *nc);
 //void broadcast_aqualinkstate_error(struct mg_connection *nc, char *msg);
 
